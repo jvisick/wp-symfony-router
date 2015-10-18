d8b141f5b986ddeaf361afbba09a4e65af05ee3e
Joshua Visick <josh@visickdesign.com>
2015-10-17 18:43:40 -0700

    Add license to plugin header

--- plugin.php
+++ plugin.php
@@ -7,6 +7,18 @@
  * Author: Josh Visick
  * Author URI: http://www.visickdesign.com
  *
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * JV WP-Symfony Router is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * JV WP-Symfony Router is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
